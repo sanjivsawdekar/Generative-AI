0e47410b39d2c76243ea940b02a1ea200fe27118
Sanjiv <s_sawdekar@yahoo.com>
2025-09-25 17:45:35 +0530

    Model Context Protocol (Client)
    FastAPI + MCP

--- MCP/mcp_client_streamable_http.py
+++ MCP/mcp_client_streamable_http.py
@@ -2,18 +2,22 @@ import asyncio
 from fastmcp.client import Client
 from fastmcp.client.transports import StreamableHttpTransport
 
+host="localhost" # IP address of host or hostname
+api_port=8000
+mcp_port=8766
+
 async def main():
-    transport = StreamableHttpTransport(url="http://localhost:8765/mcp")
+    transport = StreamableHttpTransport(url=f"http://{host}:{mcp_port}/mcp")
 
     async with Client(transport=transport) as client:
         print("\n=== Connected to MCP server ===")
 
         tools = await client.list_tools()
-        print("\n=== Available Tools ===")
-        for t in tools:
-            print(f"- {t.name}: {t.description}")
+        print("\n=== Available Tools ===\n")
+        for index, t in enumerate(tools):
+            print(f"============================\nTool#{index+1} - {t.name}:\n============================\n{t.description}\n")
 
-        res = await client.call_tool("process_json", {
+        res = await client.call_tool("process_json_func", {
                 "payload": {"user": "alice", "values": [1, 2, 3], "meta": {"active": True}}
         })
         print("\n=== Raw Result ===")
@@ -23,13 +27,29 @@ async def main():
         if res.content:
             for item in res.content[0]:
                     print(f"{item[0]}: {item[1]}")
-                    #print(item[0] , "=",item[1])
         else:
             print("No content returned")
 
         if res.structured_content:
             print("\n=== Structured JSON Output ===", type(res.structured_content))
             print(res.structured_content)
+
+
+    # ------------------------------
+    # Calling the /process_json endpoint
+    # ------------------------------
+    import requests
+
+    url = f"http://{host}:{api_port}/process_json"
+    payload = {
+        "user": "sanjiv",
+        "values": [10, 20, 30],
+        "meta": {"source": "python-client"}
+    }
+
+    response = requests.post(url, json=payload)
+    print("\n=== Endpoint Output ===\n", response.json())
+
 
 if __name__ == "__main__":
     asyncio.run(main())
